c32924472c3eb7153a657f0dd0e07b3064e3ec69
Xie Qiaoyu <darkseaon@gmail.com>
2019-06-23 22:20:56 +0800

    add cli params

--- cli/core.go
+++ cli/core.go
@@ -1,9 +1,11 @@
 package cli
 
 import (
+	"flag"
 	"fmt"
 	prompt "github.com/c-bata/go-prompt"
 	nclient "github.com/xieqiaoyu/nats-streaming-cli/client"
+	"os"
 	"strings"
 )
 
@@ -31,25 +33,57 @@ func completer(in prompt.Document) []prompt.Suggest {
 	blocks := strings.Split(input, " ")
 	return rootCmd.Complete(blocks...)
 }
+
+const usageStr = `
+Usage: nats-streaming-cli [options]
+Options:
+      -h   --host <string>           set nats-streaming server host (default: localhost)
+      -p   --port <int>              set nats-streaming server port (default: 4222)
+      -m   --http_port <int>         set http monitoring port (default: 8222)
+      -cid --cluster_id <string>     set the server cluster ID, if not set, we will try to get cluster id from server monitor endpoint
+           --client_id  <string>     specific client id cli use ,if not set, we will use a random client id
+`
+
+func usage() {
+	fmt.Printf("%s\n", usageStr)
+	os.Exit(0)
+}
+
 func Run() {
-	var port = 4222
-	var httpPort = 8222
-	var host = "localhost"
-	var clientID = ""
-	var clusterID = ""
+	fs := flag.NewFlagSet("nats-streaming-cli", flag.ExitOnError)
+	fs.Usage = usage
+	var port, httpPort uint
+	var host, clientID, clusterID string
+	fs.UintVar(&port, "p", 4222, "")
+	fs.UintVar(&port, "port", 4222, "")
+
+	fs.UintVar(&httpPort, "m", 8222, "")
+	fs.UintVar(&httpPort, "http_port", 8222, "")
+
+	fs.StringVar(&host, "h", "localhost", "")
+	fs.StringVar(&host, "host", "localhost", "")
+
+	fs.StringVar(&clientID, "client_id", "", "")
+
+	fs.StringVar(&clusterID, "cid", "", "")
+	fs.StringVar(&clusterID, "cluster_id", "", "")
+	fs.Parse(os.Args[1:])
+
 	var err error
 
 	monitor = &nclient.NatsStreamingMonitor{
 		Host:     host,
-		HttpPort: httpPort,
+		HttpPort: int(httpPort),
 	}
 	client = &nclient.NatsStreamingClient{
 		Host: host,
-		Port: port,
+		Port: int(port),
 	}
+
 	if clientID == "" {
 		clientID = nclient.GenerateClientID()
 	}
+
 	client.ID = clientID
 	if clusterID == "" {
 		clusterID, err = monitor.GetClusterID()
